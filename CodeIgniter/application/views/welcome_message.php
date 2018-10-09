f6004f9c508172a809d6bdfae76a79924cfd69e1
sjvu <sergiojaviervergara@gmail.com>
2018-10-08 21:40:15 -0300

    Prueba_V1.0

---
+++ CodeIgniter/application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Prueba</title>
+	<style>
+		body{
+			font-family: Arial, "Helveltica";
+		}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Prueba 1</h1>
+</div>
+
+</body>
+</html>
---
+++ Prueba/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hola</title>
+	<style>
+		body{
+			font-style: oblique;
+			font-family: arial
+		}
+	</style>
+</head>
+<body>
+<div>
+	<h1>Hola mundo</h1>
+
+</div>
+</body>
+</html>
